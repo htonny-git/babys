e94517d608700be5e1dd98bd2b53943ea4d94934
htonny-git <htonny@vip.qq.com>
2022-12-07 17:15:36 +0800

    商城的数据业务处理

--- index/views.py
+++ index/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from commodity.models import *
 
-# Create your views here.
+
+def indexView(request):
+    title = '首页'
+    classContent = ''
+    commodityInfos = CommodityInfos.objects.order_by('-sold').all()[:8]
+
+    types = Types.objects.all()
+    # 宝宝服饰
+    cl = [x.seconds for x in types if x.firsts == '儿童服饰']
+    clothes = CommodityInfos.objects.filter(types__in=cl).order_by('-sold')[:5]
+    # 奶粉辅食
+    fl = [x.seconds for x in types if x.firsts == '奶粉辅食']
+    food = CommodityInfos.objects.filter(types__in=fl).order_by('-sold')[:5]
+    # 宝宝用品
+    gl = [x.seconds for x in types if x.firsts == '儿童用品']
+    goods = CommodityInfos.objects.filter(types__in=gl).order_by('-sold')[:5]
+
+    return render(request, 'index.html', locals())
